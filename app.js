b94e3528e29655f3f4d0ca1c603ed29c22ed190d
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-01 18:22:26 -0600

    update: added jade engine and render index as the page

--- app.js
+++ app.js
@@ -5,11 +5,14 @@ const nodemailer = require("nodemailer");
 
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("<h1>Hello world </h1>");
+  res.render("index");
 });
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
