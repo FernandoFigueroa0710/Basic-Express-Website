027310e6ee4c6661d52ab5b4032c779cf47b545b
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-02 10:18:40 -0600

    update: nodemailer sending emails through Gmail service

--- app.js
+++ app.js
@@ -13,13 +13,57 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.render("index", { title: "Welcome" });
+  res.render("index", { title: "Computer Not Working?" });
 });
 
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.get("/contact", (req, res) => {
+  res.render("contact");
 });
+
+app.post("/contact/send", (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "youremail@email.com", // enter a valid gmail address
+      pass: "your password" // enter a valid password
+    }
+  });
+
+  const mailOptions = {
+    from: "Your Name <originemail@email.com>", //enter a valid source or origin email
+    to: "destination@email.com", // enter a landing email, destination email
+    subject: "Website Submission",
+    text:
+      "You have a submission with the following details... Name: " +
+      req.body.name +
+      "Email: " +
+      req.body.email +
+      "Message: " +
+      req.body.message,
+    html:
+      "<p>You have a submission with the following details...</p><ul><li>Name: " +
+      req.body.name +
+      "</li><li>Email: " +
+      req.body.email +
+      "</li><li>Message: " +
+      req.body.message +
+      "</li></ul>"
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.redirect("/");
+    } else {
+      console.log("Message Sent: " + info.response);
+      res.redirect("/");
+    }
+  });
+});
+
+app.listen(3000);
+console.log("Server is running on port 3000...");
